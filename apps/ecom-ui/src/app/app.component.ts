39081678da9fb401af58b513d9715bacfc8d4393
sergeemmanuelk <serge_em@outlook.fr>
2024-10-26 13:12:13 +0000

    ui layout

--- apps/ecom-ui/src/app/app.component.ts
+++ apps/ecom-ui/src/app/app.component.ts
@@ -2,10 +2,12 @@ import { Component, inject, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { FaConfig, FaIconComponent, FaIconLibrary } from '@fortawesome/angular-fontawesome';
 import { fontAwesomeIcons } from './shared/font-awesome-icons';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { FooterComponent } from "./layout/footer/footer.component";
 
 @Component({
   standalone: true,
-  imports: [RouterModule, FaIconComponent],
+  imports: [RouterModule, FaIconComponent, NavbarComponent, FooterComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
